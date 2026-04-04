41a0b2aab246d6dda7234fa94ae04a9a5dbb6026
Mohammed Alkamel <Mohammedalkamel@gmail.com>
2026-04-04 21:04:25 +0300

    Force Navigation Bar color override via Java

--- android/app/src/main/java/com/muhammad/lawyerapp/MainActivity.java
+++ android/app/src/main/java/com/muhammad/lawyerapp/MainActivity.java
@@ -1,5 +1,19 @@
 package com.muhammad.lawyerapp;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // القوة السحرية: إجبار شريط أزرار نظام الأندرويد ليكون بنفس لون شريط التطبيق
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setNavigationBarColor(Color.parseColor("#15151e"));
+    }
+}
